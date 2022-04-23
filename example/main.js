39f169bd415656736a32985dc104e6a6280a8c74
chenle <chenle@forchange.tech>
2022-04-23 10:29:49 +0800

    feat: add example

--- example/main.js
+++ example/main.js
@@ -1,1 +1,8 @@
-// TODO
+import Player from 'xgplayer';
+
+new Player({
+  id: 'mse',
+  // url: 'http://vfx.mtime.cn/Video/2019/02/04/mp4/190204084208765161.mp4'
+  url:'//sf1-cdn-tos.huoshanstatic.com/obj/media-fe/xgplayer_doc_video/mp4/xgplayer-demo-720p.mp4',
+  poster: "//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg",
+});
