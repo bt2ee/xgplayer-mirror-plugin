937ca147fab598aa61f6b6d4cc5103a0b06c3b6c
chenle <chenle@forchange.tech>
2022-05-05 22:15:19 +0800

    feat: vite 预编译

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,13 @@ import svgLoader from 'vite-svg-loader'
 export default defineConfig({
   plugins: [svgLoader({
     defaultImport: 'raw' // or 'raw'
-  })]
+  })],
+  optimizeDeps: {
+    include: ['xgplayer-mirror-plugin']
+  },
+  build: {
+    commonjsOptions: {
+      include: [/xgplayer-mirror-plugin/, /node_modules/]
+    }
+  }
 });
