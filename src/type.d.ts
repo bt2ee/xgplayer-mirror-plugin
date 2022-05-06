94c4d165aad63b0ee6ffec898eb93ad80f5d4e08
chenle <chenle@forchange.tech>
2022-05-06 22:28:43 +0800

    feat: types

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,2 +1,9 @@
 declare module 'xgplayer/src/utils/util';
 declare module '*.svg'
+declare module 'xgplayer-mirror-plugin';
+
+declare module 'xgplayer' {
+  interface IPlayerOptions {
+    mirror?: boolean | { horizontal?: boolean; vertical?: boolean }
+  }
+}
